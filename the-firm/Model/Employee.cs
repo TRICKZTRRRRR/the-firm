aabaaed299c1f7bcbb7a9ba10e8a3d881ecdf93f
Jonas Steinmejer <jonas04@outlook.dk>
2023-09-19 12:15:24 +0200

    Deletede spaces

---
+++ the-firm/Model/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace the_firm.Model
+{
+    internal class Employee
+    {
+        View.View view = new View.View();
+
+
+        protected string Fornavn { get; set; }
+        protected string Efternavn { get; set; }
+        protected string CprNummer { get; set; }
+
+        public Employee(string fornavn, string efternavn, string cprNummer)
+        {
+            Fornavn = fornavn;
+            Efternavn = efternavn;
+            CprNummer = cprNummer;
+        }
+
+        public void PrintInfo()
+        {
+            view.Name(Fornavn, Efternavn, CprNummer);
+        }
+    }
+}
---
+++ the-firm/Model/Manager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace the_firm.Model
+{
+    internal class Manager: Employee
+    {
+        View.View view = new View.View();
+
+        protected int TelefonNummer { get; set; }
+
+
+        public Manager(string fornavn, string efternavn, string cprNummer, int telefonNummer) : base(fornavn, efternavn, cprNummer)
+        {
+            TelefonNummer = telefonNummer;
+        }
+
+        public new void PrintInfo()
+        {
+            base.PrintInfo();
+            view.PhoneNumber(TelefonNummer);
+        }
+    }
+}
